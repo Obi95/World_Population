d5e36f533be9d9d61b4c9c0607835e87c6a33b2b
Omar BD <156642286+Obi95@users.noreply.github.com>
2024-06-11 18:26:20 -0500

    analysis.py

--- analysis.py
+++ analysis.py
@@ -1,36 +1,27 @@
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
+
 df = pd.read_csv("world_population.csv")
 
 # Knowing what kind of columns we have is a good way to help us think of how to look through our data. Although it might just be easy to 
 # do the same using excel for an easier understanding of our columns headers. 
 print("\nColumn headers:")
+print(df.columns, "\n")
 
-print(df.columns,"\n")
-
-# 1. Calculate the total population for each continent in 2022 rounding the percentages
+# Calculate the total population for each continent in 2022 rounding the percentages
 continent_population = df.groupby('Continent')['2022 Population'].sum().reset_index()
 total_world_population = df['2022 Population'].sum()
 continent_population['Population Percentage'] = (continent_population['2022 Population'] / total_world_population) * 100
 continent_population['Population Percentage'] = continent_population['Population Percentage'].round(2)
 continent_population_sorted = continent_population.sort_values(by='Population Percentage', ascending=False)
-print(continent_population_sorted,)
+print(continent_population_sorted)
 
-# 2. Calculate total population by continent in 2022
-continent_population = df.groupby('Continent')['2022 Population'].sum().reset_index()
 
-# 3. Calculate total world population in 2022
-total_world_population = df['2022 Population'].sum()
-
-# 4. Calculate population percentage for each continent
-continent_population['Population Percentage'] = (continent_population['2022 Population'] / total_world_population) * 100
-continent_population['Population Percentage'] = continent_population['Population Percentage'].round(2)
-continent_population_sorted = continent_population.sort_values(by='Population Percentage', ascending=False)
-
+# Calculate total world population in 2022
 total_population_2022 = df['2022 Population'].sum()
 print(" ")
 print("Earth's Population in 2022:", total_population_2022,"\n")
+
 
 # creating a pie chart
 plt.figure(figsize=(10, 8))
@@ -39,25 +30,22 @@ plt.title(f"Earth's Population in 2022: {total_population_2022:,}")
 plt.legend(title='Continents', loc='upper left')
 plt.show()
 
-# 5. Count the number of unique countries in each continent
+# Count the number of unique countries in each continent
 countries_per_continent = df.groupby('Continent')['Country/Territory'].nunique()
 print("Total unique countries per continent:\n", countries_per_continent, "\n")
 
-
-# 7. countries with the least population in 2022
+# 4. Countries with the least and highest population in 2022
 idx_least_population = df['2022 Population'].idxmin()
 idx_highest_population = df['2022 Population'].idxmax()
-# Get the country names using the indices
 country_least_population = df.loc[idx_least_population, 'Country/Territory']
 country_highest_population = df.loc[idx_highest_population, 'Country/Territory']
 print("\nCountry with the Least Population:", country_least_population)
 print("Country with the Highest Population:", country_highest_population, "\n")
 
-
+# 5. Population of countries in North America and South America
 na_sa_countries = df[df['Continent'].isin(['North America', 'South America'])]
 na_sa_population = na_sa_countries[['Country/Territory', '2022 Population']]
 na_sa_population_sorted = na_sa_population.sort_values(by='2022 Population', ascending=False)
-
 print("\nPopulation of Countries in North America and South America:")
 print(na_sa_population_sorted)
 
